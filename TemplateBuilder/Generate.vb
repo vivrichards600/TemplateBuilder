16d4d19c3619e8a477d10ecb7c21a8ba0505700b
Viv Richards <vivrichards600@hotmail.com>
2014-05-18 23:23:17 +0100

    First commit
    Tests added to ensure we receive a template, form name and valid field
    parameters with valid field parameter types.

---
+++ TemplateBuilder/Generate.vb
@@ -0,0 +1,38 @@
+﻿Public Class Generate
+    ''' <summary>
+    ''' Create a template based on input parameters
+    ''' </summary>
+    ''' <param name="inputParameters"></param>
+    ''' <returns>Creates Web Form, Web Form Code, Web Form tests, SQL Scripts</returns>
+    ''' <remarks>EXAMPLE USAGE: TemplateName FormName Field1:Varchar Field2:Int Field3:Date</remarks>
+    Public Function Template(inputParameters As String) As String
+        If String.IsNullOrWhiteSpace(inputParameters) Then Throw New Exception 'fail if we have no FieldParameters
+        If inputParameters.Split(" ").Count <= 2 Then Throw New Exception 'fail if we have only a template or form name
+
+        CheckTypes(FieldParameters(inputParameters)) 'fail if user field FieldParameters do not have valid field types
+
+        Return "generated output"
+
+    End Function
+
+    ''' <summary>
+    ''' Obtain list of field parameters
+    ''' </summary>
+    Private Function FieldParameters(ByVal usersParameters As String) As List(Of String)
+        Dim listOfFieldParameters = usersParameters.Split(" ").ToList
+        listOfFieldParameters.RemoveRange(0, 2)
+        Return listOfFieldParameters
+    End Function
+
+    ''' <summary>
+    ''' Throw exception is field parameter doesnt have a type or isn't varchar, int or date
+    ''' </summary>
+    Private Sub CheckTypes(ByVal parameters As List(Of String))
+        If parameters.Any(Function(parameter) (parameter.Contains(":")) = False) Then Throw New Exception
+
+        If parameters.Any(Function(parameter) (parameter.ToLower.Contains("varchar") _
+                                               Or parameter.ToLower.Contains("int") _
+                                               Or parameter.ToLower.Contains("date")) = False) Then Throw New Exception
+    End Sub
+
+End Class
---
+++ TemplateBuilderTests/GenerateTemplateUnitTests.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Text
+Imports TemplateBuilder
+
+<TestClass()>
+Public Class GenerateTemplateUnitTests
+
+    <TestMethod()>
+    <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_When_Generating_Template_Code_But_No_Parameters_Input()
+        Generate.Template(NO_PARAMETERS)
+    End Sub
+
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_If_Input_Parameters_Only_Contains_Template_Name()
+        Generate.Template(FORM_TEMPLATE_PARAMETER)
+    End Sub
+
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_If_Input_Parameters_Only_Contains_Template_And_Form_Name()
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER)
+    End Sub
+
+    <TestMethod()> _
+    <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_If_Input_Parameters_Contains_Invalid_Field_Parameter()
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + FORM_FIELD_INVALID_PARAMETER)
+    End Sub
+
+    <TestMethod()>
+    Public Sub Display_Generated_Template_Code_When_Parameters_Field_With_Type_Varchar_Input()
+        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
+
+        Assert.AreEqual(GENERATED_OUTPUT, EXPECTED_GENERATED_OUTPUT)
+    End Sub
+
+    <TestMethod()>
+    Public Sub Display_Generated_Template_Code_When_Parameters_Field_With_Type_Int_Input()
+        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
+
+        Assert.AreEqual(GENERATED_OUTPUT, EXPECTED_GENERATED_OUTPUT)
+    End Sub
+
+    <TestMethod()>
+    Public Sub Display_Generated_Template_Code_When_Parameters_Field_With_Type_Date_Input()
+        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
+
+        Assert.AreEqual(GENERATED_OUTPUT, EXPECTED_GENERATED_OUTPUT)
+    End Sub
+
+
+    Public Shared Generate As Generate
+
+    <TestInitialize()>
+    Public Sub Initialization()
+        Generate = New Generate
+    End Sub
+
+    Dim NO_PARAMETERS = String.Empty
+    Dim FORM_TEMPLATE_PARAMETER = "TemplateName "
+    Dim FORM_NAME_PARAMETER = "FormName "
+    Dim FORM_FIELD_INVALID_PARAMETER = "Field:Type "
+    Dim INPUT_PARAMETERS = "TemplateName FormName Field1:VaRcHar Field2:InT Field3:DatE"
+    Dim EXPECTED_GENERATED_OUTPUT = "generated output"
+    Dim GENERATED_OUTPUT As String = String.Empty
+
+
+End Class
