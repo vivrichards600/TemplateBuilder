bc5c348d9a6357fd3d0d17da317b84ea5fcd43ea
Viv Richards <vivrichards600@hotmail.com>
2014-05-26 22:40:36 +0100

    tests added, refactoring
    tests for parameter field size added, code refactored

--- TemplateBuilder/Generate.vb
+++ TemplateBuilder/Generate.vb
@@ -1,5 +1,4 @@
 ﻿Public Class Generate
-
     ''' <summary>
     ''' Create a template based on input parameters
     ''' </summary>
@@ -7,14 +6,21 @@
     ''' <returns>Creates Web Form, Web Form Code, Web Form tests, SQL Scripts</returns>
     ''' <remarks>EXAMPLE USAGE: TemplateName FormName Field1:Varchar Field2:Int Field3:Date</remarks>
     Public Function Template(inputParameters As String) As String
-        If String.IsNullOrWhiteSpace(inputParameters) Then Throw New Exception 'fail if we have no FieldParameters
-        If inputParameters.Split(" ").Count <= 2 Then Throw New Exception 'fail if we have only a template or form name
+        If String.IsNullOrWhiteSpace(inputParameters) Then Throw New Exception("no field parameter input")
+        If inputParameters.Split(" ").Count <= 2 Then Throw New Exception("template of form name missing")
 
-        CheckTypes(FieldParameters(inputParameters)) 'fail if user field FieldParameters do not have valid field types
+        CheckTypes(FieldParameters(inputParameters))
+        CheckSize(FieldParameters(inputParameters))
+        'fail if user field FieldParameters do not have valid field types
+
+
+
+
 
         Return "<%@ Page Language=""vb"" AutoEventWireup=""false"" CodeBehind=""ProjectName.aspx.vb"" Inherits=""FormName.TemplateName"" %>"
 
     End Function
+
 
     ''' <summary>
     ''' Obtain list of field parameters
@@ -22,18 +28,41 @@
     Private Function FieldParameters(ByVal usersParameters As String) As List(Of String)
         Dim listOfFieldParameters = usersParameters.Split(" ").ToList
         listOfFieldParameters.RemoveRange(0, 2)
+
         Return listOfFieldParameters
     End Function
 
     ''' <summary>
-    ''' Throw exception is field parameter doesnt have a type or isn't varchar, int or date
+    ''' Check field parameter contains valid field type
     ''' </summary>
     Private Sub CheckTypes(ByVal parameters As List(Of String))
-        If parameters.Any(Function(parameter) (parameter.Contains(":")) = False) Then Throw New Exception
+        If parameters.Any(Function(parameter) (parameter.Contains(":")) = False) Then Throw New Exception("field parameter requires a field name as well as a field type seperated with a :")
 
         If parameters.Any(Function(parameter) (parameter.ToLower.Contains("varchar") _
                                                Or parameter.ToLower.Contains("int") _
-                                               Or parameter.ToLower.Contains("date")) = False) Then Throw New Exception
+                                               Or parameter.ToLower.Contains("date")) = False) Then Throw New Exception("invalid type input for field parameter")
     End Sub
+
+    ''' <summary>
+    ''' Check field parameter contains a valid size if it's specified
+    ''' </summary>
+    ''' <param name="inputParameters"></param>
+    ''' <remarks></remarks>
+    Private Sub CheckSize(ByVal parameters As List(Of String))
+        parameters.ForEach(Sub(parameter)
+
+                               If parameter.Contains("(") = True And parameter.Contains(")") = True Then
+                                   parameter = parameter.Replace("(", "")
+                                   parameter = parameter.Replace(")", "")
+                                   If IsNumeric(parameter) = False Then Throw New Exception("found an opening and closing bracket for field size but no number")
+                               End If
+
+                               If parameter.Contains("(") = True And parameter.Contains(")") = False Then Throw New Exception("found an opening bracket for field size but no closing bracket")
+                               If parameter.Contains("(") = False And parameter.Contains(")") = True Then Throw New Exception("found a closing bracket for field size but no opening bracket")
+                           End Sub)
+    End Sub
+
+
+
 
 End Class
--- TemplateBuilderTests/GenerateTemplateUnitTests.vb
+++ TemplateBuilderTests/GenerateTemplateUnitTests.vb
@@ -25,28 +25,31 @@ Public Class GenerateTemplateUnitTests
     <TestMethod()> _
     <ExpectedException(GetType(System.Exception))> _
     Public Sub Throw_Exception_If_Input_Parameters_Contains_Invalid_Field_Parameter()
-        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + INVALID_FORM_FIELD_PARAMETER)
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + FORM_FIELD_INVALID_PARAMETER)
     End Sub
 
     <TestMethod()>
-    Public Sub Display_Generated_Template_Code_When_Parameters_Field_With_Type_Varchar_Input()
-        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
-
-        Assert.AreEqual(EXPECTED_GENERATED_OUTPUT, GENERATED_OUTPUT)
+    <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_When_Parameters_Field_Contains_Invalid_Data_Type()
+        Generate.Template(INVALID_INPUT_PARAMETERS)
     End Sub
 
     <TestMethod()>
-    Public Sub Display_Generated_Template_Code_When_Parameters_Field_With_Type_Int_Input()
-        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
-
-        Assert.AreEqual(EXPECTED_GENERATED_OUTPUT, GENERATED_OUTPUT)
+      <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_When_Parameters_Field_Contains_Field_Size_Without_Closing_Bracket()
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + "Field1:Varchar(200")
     End Sub
 
     <TestMethod()>
-    Public Sub Generate_Template_Code_When_Parameters_Field_With_Type_Date_Input()
-        GENERATED_OUTPUT = Generate.Template(INPUT_PARAMETERS)
+   <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_When_Parameters_Field_Contains_Field_Size_Without_Opening_Bracket()
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + "Field1:Varchar200)")
+    End Sub
 
-        Assert.AreEqual(EXPECTED_GENERATED_OUTPUT, GENERATED_OUTPUT)
+    <TestMethod()>
+   <ExpectedException(GetType(System.Exception))> _
+    Public Sub Throw_Exception_When_Parameters_Field_Contains_Field_Size_Opening_And_Closing_Brackets_But_No_Size()
+        Generate.Template(FORM_TEMPLATE_PARAMETER + FORM_NAME_PARAMETER + "Field1:Varchar()")
     End Sub
 
     <TestMethod()>
@@ -55,7 +58,6 @@ Public Class GenerateTemplateUnitTests
 
         Assert.AreEqual(EXPECTED_GENERATED_OUTPUT, GENERATED_OUTPUT)
     End Sub
-
 
     Public Shared Generate As Generate
 
@@ -67,8 +69,9 @@ Public Class GenerateTemplateUnitTests
     Dim NO_PARAMETERS = String.Empty
     Dim FORM_TEMPLATE_PARAMETER = "TemplateName "
     Dim FORM_NAME_PARAMETER = "FormName "
-    Dim INVALID_FORM_FIELD_PARAMETER = "Field:Type "
+    Dim FORM_FIELD_INVALID_PARAMETER = "Field:Type "
     Dim INPUT_PARAMETERS = "TemplateName FormName Field1:VaRcHar Field2:InT Field3:DatE"
+    Dim INVALID_INPUT_PARAMETERS = "TemplateName FormName Field1:SomeInvalidType"
     Dim EXPECTED_GENERATED_OUTPUT = "<%@ Page Language=""vb"" AutoEventWireup=""false"" CodeBehind=""ProjectName.aspx.vb"" Inherits=""FormName.TemplateName"" %>"
     Dim GENERATED_OUTPUT As String = String.Empty
 
