e626fd76549347a8c3eb45353561c478195f3a9c
Viv Richards <vivrichards600@hotmail.com>
2014-07-07 21:09:46 +0100

    selenium integration test added to ensure page displays

---
+++ TemplateBuilderTests/TemplateBuilderFormIntegrationSpec.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Firefox
+Imports OpenQA.Selenium.IE
+
+<TestClass()>
+Public Class TemplateBuilderFormIntegrationSpec
+
+    Private testContextInstance As TestContext
+    Public Shared WebBrowser As IWebDriver
+
+    '''<summary>
+    '''Gets or sets the test context which provides
+    '''information about and functionality for the current test run.
+    '''</summary>
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(ByVal value As TestContext)
+            testContextInstance = value
+        End Set
+    End Property
+
+    <TestInitialize()>
+    Public Sub Initialization()
+        WebBrowser = New FirefoxDriver
+    End Sub
+
+    <TestCleanup()>
+    Public Sub Termination()
+        WebBrowser.Quit()
+    End Sub
+
+    ''' <summary>
+    ''' Ensure that the page is presented
+    ''' </summary>
+    ''' <remarks></remarks>
+    <TestMethod()> _
+    Public Sub PageIsPresented()
+
+        'Arrange & Act
+        WebBrowser.Navigate.GoToUrl("http://localhost:52095/TemplateBuilderForm.aspx")
+
+        'Assert
+        Assert.AreEqual(WebBrowser.Title, "Template Builder")
+
+    End Sub
+
+End Class
