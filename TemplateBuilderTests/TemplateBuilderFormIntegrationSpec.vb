0741ffcb849f9e112aa50fb9d92c75425998c31f
Viv Richards <vivrichards600@hotmail.com>
2014-07-21 21:27:32 +0100

    Integration tests added to check for form errors
    Tests added to ensure when template builders loads and no params input
    we get the correct error as well as when a user inputs only a form name
    we should get an error.

--- TemplateBuilderTests/TemplateBuilderFormIntegrationSpec.vb
+++ TemplateBuilderTests/TemplateBuilderFormIntegrationSpec.vb
@@ -37,8 +37,7 @@ Public Class TemplateBuilderFormIntegrationSpec
     ''' </summary>
     ''' <remarks></remarks>
     <TestMethod()> _
-    Public Sub PageIsPresented()
-
+    Public Sub Page_Is_Presented()
         'Arrange & Act
         WebBrowser.Navigate.GoToUrl("http://localhost:52095/TemplateBuilderForm.aspx")
 
@@ -47,4 +46,28 @@ Public Class TemplateBuilderFormIntegrationSpec
 
     End Sub
 
+    <TestMethod()> _
+    Public Sub Page_Displays_Input_Parameters_Are_Mandatory_Error_When_No_Parameters_Input_And_Generate_Template_Button_Clicked()
+        'Arrange
+        WebBrowser.Navigate.GoToUrl("http://localhost:52095/TemplateBuilderForm.aspx")
+
+        'Act
+        WebBrowser.FindElement(By.Id("GenerateTemplateButton")).Click()
+
+        'Assert
+        Assert.IsTrue(WebBrowser.PageSource.Contains("Input parameters are mandatory and must contain i.e ProjectName TemplateName Field:Type(OptionalSize)"))
+    End Sub
+
+    <TestMethod()> _
+    Public Sub Page_Displays_Template_Or_Form_Name_Missing_Error_When_Only_A_Project_Name_Input_And_Generate_Template_Button_Clicked()
+        'Arrange
+        WebBrowser.Navigate.GoToUrl("http://localhost:52095/TemplateBuilderForm.aspx")
+        WebBrowser.FindElement(By.Id("InputParametersTextBox")).SendKeys("ProjectName")
+
+        'Act
+        WebBrowser.FindElement(By.Id("GenerateTemplateButton")).Click()
+
+        'Assert
+        Assert.IsTrue(WebBrowser.PageSource.Contains("Input parameters are mandatory and must contain i.e ProjectName TemplateName Field:Type(OptionalSize)"))
+    End Sub
 End Class
